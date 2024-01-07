f4f09260c4b108da7d9e39363f5b05caa56ba03d
moeschia <alexandre.moesching@roche.com>
2024-01-07 15:59:14 +0100

    Added test of compare_norm function

--- exec/Test_QATS_vs_Viterbi_norm.R
+++ exec/Test_QATS_vs_Viterbi_norm.R
@@ -16,6 +16,25 @@ summary(res$res_Vit)
 summary(res$res_QATS)
 
 ################################
+rm(list = ls())
+library("QATS")
+
+n <- 1e5 + 1; m <- 5; K <- 10; sigma <- 0.5
+
+Pi <- rep(1, m) / m
+pp <- matrix(K / ((n - 1) * (m - 1)), nrow = m, ncol = m)
+diag(pp) <- 1 - K / (n - 1)
+
+d0 <- 3; n_seeds <- 5; rotate <- FALSE; n_rep <- 1e1; n_sim <- 1e2
+
+res <- compare_norm(n, m, Pi, pp, sigma,
+                    d0, n_seeds, rotate, n_rep, n_sim)
+
+summary(res$res_Vit)
+summary(res$res_PMAP)
+summary(res$res_QATS)
+
+################################
 
 rm(list = ls())
 library("QATS")
